1ee59208128822d0f4e76560db26472376711f82
Md.Mainul Islam <u1902141@student.cuet.ac.bd>
2020-08-22 04:30:33 +0600

    first react explore

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person={name:"Mahfuzur benson",
+  phone:"01531542960"}
+  var style={
+    color:'red',
+    backgroundColor:'salmon'
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +19,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+  <h1 style={style}>Here comes:{person.name+" "+person.phone}</h1>
+        
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
